fb7ef124a6645b212aaf668579a6c30b8417f796
carlosguerra72 <carlosguerra72@hotmail.com>
2022-04-23 14:39:34 -0500

    hice suma, casi resto y principio del si

---
+++ calculadora/operaciones/Resta.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace operaciones
+{
+    class Resta
+    {
+    }
+}
---
+++ calculadora/operaciones/Suma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace operaciones
+{
+    public class Suma
+    {
+        public int numera { get; set; }
+        public int numerb { get; set; }
+        public int numerc { get; set; }
+
+        public void sumar()
+        {
+            numerc = numera + numerb;        
+        }
+    }
+}
